196626637b91427cd31aa15803f92fc94ff87195
AK261102 <ak503005@gmail.com>
2025-08-31 03:54:11 +0530

    Add hybrid inheritance example

--- OOPS/hyrid.java
+++ OOPS/hyrid.java
@@ -1,3 +1,38 @@
+class Animal{
+    void eat()
+    {
+        System.out.println("animal hu mein");
+    }
+}
+
+interface Mammal{
+    void walk();
+}
+interface Pet{
+    void play();
+}
+
+class Dog extends Animal implements Mammal, Pet{
+    @Override
+    void eat(){
+        System.out.println("Eating");
+    }
+    @Override
+    public void walk()
+    {
+        System.out.println("this dog walks");
+    }
+    public void play()
+    {
+        System.out.println("I am walking");
+    }
+}
 public class hyrid {
-    
+    public static void main(String [] args)
+    {
+        Dog dog=new Dog();
+        dog.eat();
+        dog.walk();
+        dog.play();
+    }
 }
