f4644d6ab8373bdeacb0cedad1cc01cfbf90dfdd
AK261102 <ak503005@gmail.com>
2025-10-22 00:41:39 +0530

    starting with Composite design , Bad code

---
+++ Structural-Design-Patterns/Composite_Design_Pattern/Bad_Code/AirConditioner.java
@@ -0,0 +1,16 @@
+package Structural-Design-Patterns.Composite_Design_Pattern.Bad_Code;
+
+public class AirConditioner {
+    public AirConditioner()
+    {
+
+    }
+    void turnOn()
+    {
+        System.out.println("Turn on the airConditioner");
+    }
+    void turnOff()
+    {
+        System.out.println("Turn off the Air Conditioner");
+    }
+}
---
+++ Structural-Design-Patterns/Composite_Design_Pattern/Bad_Code/SmartLight.java
@@ -0,0 +1,16 @@
+package Structural-Design-Patterns.Composite_Design_Pattern.Bad_Code;
+
+public class SmartLight {
+    public SmartLight()
+    {
+
+    }
+    void turnOn()
+    {
+        System.out.println("turn on the smartLight");
+    }
+    void turnOff()
+    {
+        System.out.println("turn off the smartlight");
+    }
+}
